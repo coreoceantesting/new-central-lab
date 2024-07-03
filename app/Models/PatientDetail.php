20d9051aa00faca82f5b1b24da2d25a043966edf
coreoceantesting <coreoceantesting@gmail.com>
2024-07-03 12:40:22 +0530

    add new colm in patient details table

--- app/Models/PatientDetail.php
+++ app/Models/PatientDetail.php
@@ -11,7 +11,7 @@ class PatientDetail extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $fillable = ['patient_id', 'health_post_name', 'patient_uniqe_id', 'first_name', 'middle_name', 'last_name', 'mob_no', 'aadhar_no', 'age', 'gender', 'address', 'main_category_id', 'tests', 'lab', 'refering_doctor_name', 'date', 'created_by', 'created_at', 'updated_by', 'updated_at', 'status', 'remark', 'patient_status', 'deleted_by', 'deleted_at', 'received_by', 'received_at', 'first_approval_status', 'first_approval_remark', 'first_approval_by', 'first_approval_at', 'second_approval_status', 'second_approval_remark', 'second_approval_by', 'second_approval_at'];
+    protected $fillable = ['patient_id', 'health_post_name', 'health_post_id' , 'patient_uniqe_id', 'first_name', 'middle_name', 'last_name', 'mob_no', 'aadhar_no', 'age', 'gender', 'address', 'main_category_id', 'tests', 'lab', 'refering_doctor_name', 'date', 'created_by', 'created_at', 'updated_by', 'updated_at', 'status', 'remark', 'patient_status', 'deleted_by', 'deleted_at', 'received_by', 'received_at', 'first_approval_status', 'first_approval_remark', 'first_approval_by', 'first_approval_at', 'second_approval_status', 'second_approval_remark', 'second_approval_by', 'second_approval_at'];
 
     public function labName()
     {
